b17f910ec6226f88756ad750bd2b0c0133461024
Polina Yemelianenkova <145330202+polina-yemelianenkova@users.noreply.github.com>
2026-05-13 18:35:05 +0000

    feat: update project creation to use collaborator_memberships for project members

--- logic/project_manager.py
+++ logic/project_manager.py
@@ -43,7 +43,7 @@ class ProjectManager:
                 name = name,
                 description = description,
                 owner_id = owner_id,
-                project_members = [ProjectMember(user_id = user.id)],
+                collaborator_memberships=[ProjectMember(user_id=user.id)],
             )
             self.session.add(project)
             self.session.commit()
