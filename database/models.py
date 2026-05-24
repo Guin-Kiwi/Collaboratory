31f2ff8787873ca420b8843660005b3b39e01dba
Polina Yemelianenkova <145330202+polina-yemelianenkova@users.noreply.github.com>
2026-05-24 11:13:29 +0000

    Refactor Assignment model to remove type hint from assigned_at field

--- database/models.py
+++ database/models.py
@@ -7,7 +7,7 @@ from .mixins import TimestampMixin
 import datetime
 
 from sqlalchemy import Boolean, DateTime, Integer, func
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy.orm import DeclarativeBase
 from sqlalchemy import (
     Column, String, Text,
     ForeignKey, Enum, UniqueConstraint
@@ -188,7 +188,7 @@ class Assignment(BaseModel):
     id          = Column(Integer, primary_key=True, index=True)
     task_id     = Column(Integer, ForeignKey("tasks.id"), nullable=False)
     user_id     = Column(Integer, ForeignKey("users.id"), nullable=False)
-    assigned_at: Mapped[datetime.datetime] = Column(
+    assigned_at = Column(
         DateTime(timezone=True),
         server_default=func.now(),
         nullable=False,
